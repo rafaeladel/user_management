01d9201b8957c308ca0aaa2a7bbe960427c1d000
rafaeladel <rafael.adel20@gmail.com>
2013-11-21 15:41:05 +0200

    Fixed jQuery and CSS selectors

--- src/Tsk/UserBundle/Resources/public/js/admin_panel.js
+++ src/Tsk/UserBundle/Resources/public/js/admin_panel.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-    $("#activate_form, #make_admin_form, #delete_form").submit(function($event){
-        var form_id = $event.target.id;
-        var form = $("#" + form_id);
+    $(".activate_form, .make_admin_form, .delete_form").submit(function($event){
+        var form_class = $event.target.id;
+        var form = $("." + form_class);
         var url = form.attr("action");
         form.children("button").attr("disabled","disabled");
-        if(form_id == "activate_form")
+        if(form_class == "activate_form")
         {
             form.siblings("span").text("loading..").css("color","black");
         }
         $.post(url, {}, function(data){
             if(data.code == 200)
             {
-                if(form_id == "activate_form")
+                if(form_class == "activate_form")
                 {
                     processActivateForm(form, data);
                 }
-                else if(form_id == "make_admin_form")
+                else if(form_class == "make_admin_form")
                 {
                     form.parentsUntil("tbody", "tr").find("button").attr("disabled","disabled");
                 }
-                else if(form_id =="delete_form")
+                else if(form_class =="delete_form")
                 {
                     form.parentsUntil("tbody", "tr").remove();
                 }
